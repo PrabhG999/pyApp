10bfa19f04a58f99e957530bdceff3464758feb0
Kabir Singh <prabhjeezizoo11@gmail.com>
2024-03-14 17:08:55 -0700

    all call working except buy car bcs it asks for a bearer token

--- webApp/crud/views.py
+++ webApp/crud/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login
 from django.core.mail import send_mail
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Cars
